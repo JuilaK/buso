66fd7f6f53174f00381d532b108b2c7978d762ae
Julia Karavaeva <kbazya@mail.ru>
2022-05-24 11:59:04 +0300

    Add file input

--- sendmail.php
+++ sendmail.php
@@ -1,51 +1,58 @@
 <?php
     use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
     use PHPMailer\PHPMailer\Exception;
 
-    require 'phpmailer/PHPMailer.php';
-    require 'phpmailer/Exception.php';
+    require 'phpmailer/src/PHPMailer.php';
+    require 'phpmailer/src/SMTP.php';
+    require 'phpmailer/src/Exception.php';
 
     $mail = new PHPMailer(true);
-    $mail->CharSet = 'UTF-8';
-    $mail->IsHTML(true);
 
-    //От кого письмо
-    $mail->setFrom('offerspb@mail.ru', 'BUSO. скупка оборудования');
-    //Кому отправить
-    $mail->addAddress('kbazya@mail.ru');
-    //Тема письма
-    $mail->Subject = 'Новая заявка - busospb.ru';
+    try {
+        $mail->isSMTP();                   
+        $mail->Host   = 'smtp.mail.ru';  
+        $mail->SMTPAuth   = true;          
+        $mail->Username   = 'buso12345@mail.ru';       
+        //$mail->Password   = 'YRsr_2AYpot3';  
+        $mail->Password   = 'R5wy3NeQVMbTRZKHm0As';  
+        $mail->SMTPSecure = 'ssl';         
+        $mail->Port   = 465;
 
-    //Тело письма
-    $body = '<h1>Новая заявка busospb.ru</h1>';
-
-    if(trim(!empty($_POST['name']))){
-        $body.= '<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+        $mail->CharSet = 'UTF-8';
+        $mail->setLanguage('ru', 'phpmailer/language/');
+        $mail->IsHTML(true);
+    
+        //От кого письмо
+        $mail->setFrom('buso12345@mail.ru', 'BUSO. Cкупка оборудования');
+        //Кому отправить
+        $mail->addAddress('kbazya@mail.ru');
+        //Тема письма
+        $mail->Subject = 'Новая заявка - busospb.ru';
+    
+        //Тело письма
+        $body = '<h1>Новая заявка busospb.ru</h1>';
+    
+        if(trim(!empty($_POST['name']))){
+            $body.= '<p><strong>Имя:</strong> '.$_POST['name'].'</p>';
+        }
+    
+        if(trim(!empty($_POST['email']))){
+            $body.= '<p><strong>Электронная почта:</strong> '.$_POST['email'].'</p>';
+        }
+    
+        if(trim(!empty($_POST['phone']))){
+            $body.= '<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
+        }
+    
+        if(trim(!empty($_POST['message']))){
+            $body.= '<p><strong>Комментарий:</strong> '.$_POST['message'].'</p>';
+        }
+    
+        $mail->Body = $body;
+        $mail->send();
+        echo 'Данные отправлены!';
+    } catch (Exception $e) {
+        echo "Данные не могут быть отправлены. Ошибка: {$mail->ErrorInfo}";
     }
-
-    if(trim(!empty($_POST['email']))){
-        $body.= '<p><strong>Электронная почта:</strong> '.$_POST['email'].'</p>';
-    }
-
-    if(trim(!empty($_POST['phone']))){
-        $body.= '<p><strong>Телефон:</strong> '.$_POST['phone'].'</p>';
-    }
-
-    if(trim(!empty($_POST['message']))){
-        $body.= '<p><strong>Комментарий:</strong> '.$_POST['message'].'</p>';
-    }
-
-    $mail->Body = $body;
-
-    //Отправка
-    /*if (!$mail->send()){
-        $message = 'Ошибка отправки данных формы';
-    } else {
-        $message = 'Данные отправлены!';
-    }
-
-    $response = ['message => $message'];
-    header('Content-type: application/json');
-    echo json_encode($response);*/
-    echo $_POST['phone'];
 ?>
